e362e88258fa6c9ed58c298910846522c58068d5
Boyjonny05 <Void59315@gmail.com>
2025-12-15 15:02:48 +0000

    Random thingies

--- day_1_activities/1_pre_lesson.py
+++ day_1_activities/1_pre_lesson.py
@@ -1,1 +1,18 @@
 # this is what we will use for the video intro to dictionaries
+# ordered amd changable. No duplicates
+
+capitals = {"USA": "Washington D.C.",
+            "India": "New Delhi",
+            "China": "Beijing",
+            "Russia": "Moscow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+# print(capitals.get("Japan"))
+
+
+
+
+
+
+keys = capitals.keys()
--- day_1_activities/2_main.py
+++ day_1_activities/2_main.py
@@ -44,9 +44,14 @@ import student_data
 # print(student_data.students)
 students = student_data.students
 print(len(students))
-print(students[0]['Combo,Name'])
-print(students[0]['Email'][0])
-print(students[0]['Email'][1])
+# print(students[0]['Combo,Name'])
+# print(students[0]['Email'][0])
+# print(students[0]['Email'][1])
+# print(students[0]["FName"][0])
+# print(students[0]["LName"][0])
+# print(students[0]["HR"][0])
+# print(students[2]["Email"][2])
+print("_"*25)
 
 # What is being counted?
 
@@ -68,11 +73,11 @@ print(students[0]['Email'][1])
 #and printing the name and email of the students
 #we are also printing a line of underscores to separate the students
 #we are also printing a line of underscores to separate the students
-for student in students:
-    print(student['Combo,Name'])
-    print(student['Email'][0])
-    print(student['Email'][1])
-    print("_"*25)
+# for student in students:
+#     print(student['Combo,Name'])
+#     print(student['Email'][0])
+#     print(student['Email'][1])
+#     print("_"*25)
 
 # What does the loop variable student represent on each pass?
 
@@ -88,11 +93,11 @@ for student in students:
 # we are asking the user to input their name
 # then we are checking if the name is in the data
 # if the name is in the data we are printing the name and "this works"
-name = input("what is you name?") 
-for student in students:
-    if name == student['Combo,Name']:
-        print(student['Combo,Name'])
-        print("this works")
+# name = input("what is you name?") 
+# for student in students:
+#     if name == student['Combo,Name']:
+#         print(student['Combo,Name'])
+#         print("this works")
 
 # What is the goal of this search?
 
